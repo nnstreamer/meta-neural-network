0afb8b18ac596202f39dd7a4e4aa9e5017ce6264
Yongjoo Ahn <yongjoo1.ahn@samsung.com>
2020-12-14 10:36:21 +0900

    [NNS/test] Add test for tensorflow_lite subplugin
    - Add `unittest_tizen_tensorflow_lite`
    - Add `unittest_tizen_tensorflow_lite-set`
    
    Signed-off-by: Yongjoo Ahn <yongjoo1.ahn@samsung.com>

--- lib/oeqa/runtime/cases/nnstreamer.py
+++ lib/oeqa/runtime/cases/nnstreamer.py
@@ -7,29 +7,43 @@ from oeqa.core.decorator.oetimeout import OETimeout
 class NNStreamerTest(OERuntimeTestCase):
 
     def test_nnstreamer(self):
-        cmd = 'export LD_LIBRARY_PATH=/usr/lib/gstreamer-1.0:$LD_LIBRARY_PATH; ' \
+        cmd = 'LD_LIBRARY_PATH=/usr/lib/gstreamer-1.0:$LD_LIBRARY_PATH ' \
         '/usr/lib/nnstreamer/unittest/unittest_common'
         (status, output) = self.target.run(cmd)
         msg = " NNSTREAMER UNITTEST FAILED ( unittest_common ): %s" % output
         self.assertEqual(status, 0, msg=msg)
 
-        cmd = 'export LD_LIBRARY_PATH=/usr/lib/gstreamer-1.0:$LD_LIBRARY_PATH; ' \
+        cmd = 'LD_LIBRARY_PATH=/usr/lib/gstreamer-1.0:$LD_LIBRARY_PATH ' \
         '/usr/lib/nnstreamer/unittest/unittest_sink -d /usr/lib/nnstreamer/customfilters --gst-plugin-path=/usr/lib/gstreamer-1.0'
         (status, output) = self.target.run(cmd)
         msg = " NNSTREAMER UNITTEST FAILED ( unittest_sink ): %s" % output
         self.assertEqual(status, 0, msg=msg)
 
-        cmd = 'export LD_LIBRARY_PATH=/usr/lib/gstreamer-1.0:$LD_LIBRARY_PATH; ' \
-        'export NNSTREAMER_SOURCE_ROOT_PATH=/usr/lib/nnstreamer/unittest;' \
+        cmd = 'LD_LIBRARY_PATH=/usr/lib/gstreamer-1.0:$LD_LIBRARY_PATH ' \
+        'NNSTREAMER_SOURCE_ROOT_PATH=/usr/lib/nnstreamer/unittest ' \
         '/usr/lib/nnstreamer/unittest/unittest_plugins --gst-plugin-path=/usr/lib/gstreamer-1.0'
         (status, output) = self.target.run(cmd)
         msg = " NNSTREAMER UNITTEST FAILED ( unittest_plugins ): %s" % output
         self.assertEqual(status, 0, msg=msg)
 
-        cmd = 'export LD_LIBRARY_PATH=/usr/lib/gstreamer-1.0:$LD_LIBRARY_PATH; ' \
+        cmd = 'LD_LIBRARY_PATH=/usr/lib/gstreamer-1.0:$LD_LIBRARY_PATH ' \
         '/usr/lib/nnstreamer/unittest/unittest_src_iio --gst-plugin-path=/usr/lib/gstreamer-1.0'
         (status, output) = self.target.run(cmd)
         msg = " NNSTREAMER UNITTEST FAILED ( unittest_src_iio ): %s" % output
+        self.assertEqual(status, 0, msg=msg)
+
+        cmd = 'LD_LIBRARY_PATH=/usr/lib/gstreamer-1.0:$LD_LIBRARY_PATH ' \
+        'NNSTREAMER_SOURCE_ROOT_PATH=/usr/lib/nnstreamer/unittest ' \
+        '/usr/lib/nnstreamer/unittest/unittest_tizen_tensorflow_lite --gst-plugin-path=/usr/lib/gstreamer-1.0'
+        (status, output) = self.target.run(cmd)
+        msg = " NNSTREAMER UNITTEST FAILED ( unittest_tizen_tensorflow_lite ): %s" % output
+        self.assertEqual(status, 0, msg=msg)
+
+        cmd = 'LD_LIBRARY_PATH=/usr/lib/gstreamer-1.0:$LD_LIBRARY_PATH ' \
+        'NNSTREAMER_SOURCE_ROOT_PATH=/usr/lib/nnstreamer/unittest ' \
+        '/usr/lib/nnstreamer/unittest/unittest_tizen_tensorflow_lite-set --gst-plugin-path=/usr/lib/gstreamer-1.0'
+        (status, output) = self.target.run(cmd)
+        msg = " NNSTREAMER UNITTEST FAILED ( unittest_tizen_tensorflow_lite-set ): %s" % output
         self.assertEqual(status, 0, msg=msg)
 
         # You may not want to do SSAT test. It takes VERY LONG TIME to be done.
